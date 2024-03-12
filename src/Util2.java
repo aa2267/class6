30b49392dfb6c0d4c2c51ef40f6bb2fb951c4781
aa2267 <160629093+aa2267@users.noreply.github.com>
2024-03-12 10:47:33 +0000

    Update Util2.java

--- src/Util2.java
+++ src/Util2.java
@@ -5,7 +5,7 @@ public class Util2 {
 	// Prints an integer array in the format [1, 2, 3]
 	public static void printArray(int[] arr) {
 		System.out.print("[");
-		for(int i = 0; i <= arr.length; i++) {
+		for(int i = 0; i < arr.length; i++) {
 			if(i > 0)
 				System.out.print(", ");
 			System.out.print(arr[i]);
