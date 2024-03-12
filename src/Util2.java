1c996dc7b0cf3e33409df17c39c842bd37dad0e3
aa2267 <160629093+aa2267@users.noreply.github.com>
2024-03-12 10:38:13 +0000

    Update Util2.java

--- src/Util2.java
+++ src/Util2.java
@@ -6,7 +6,7 @@ public class Util2 {
 	public static void printArray(int[] arr) {
 		System.out.print("[");
 		for(int i = 0; i <= arr.length; i++) {
-			if(i >= 0)
+			if(i > 0)
 				System.out.print(", ");
 			System.out.print(arr[i]);
 		}
